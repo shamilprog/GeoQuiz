aff967bdc71eae4c4f511b12567c99624ec6c7f7
Shamilprog <shamilprog@yandex.ru>
2020-10-12 01:01:25 +0300

    Creating a Question class

---
+++ app/src/main/java/ru/shamilprog/android/geoquiz/Question.java
@@ -0,0 +1,12 @@
+package ru.shamilprog.android.geoquiz;
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+}

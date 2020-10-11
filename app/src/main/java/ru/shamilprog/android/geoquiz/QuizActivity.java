81f26856a7fb9bb7e9d78defbb8f288af8c89b88
Shamilprog <shamilprog@yandex.ru>
2020-10-12 00:45:17 +0300

    Making the two toasts appear at the top of the screen

--- app/src/main/java/ru/shamilprog/android/geoquiz/QuizActivity.java
+++ app/src/main/java/ru/shamilprog/android/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package ru.shamilprog.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -20,18 +21,22 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
-                                R.string.correct_toast,
-                                Toast.LENGTH_SHORT).show();
+                Toast toast = Toast.makeText(QuizActivity.this,
+                                              R.string.correct_toast,
+                                              Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP, 0, 200);
+                toast.show();
             }
         });
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
-                                R.string.incorrect_toast,
-                                Toast.LENGTH_SHORT).show();
+                Toast toast = Toast.makeText(QuizActivity.this,
+                        R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP, 0, 200);
+                toast.show();
             }
         });
     }

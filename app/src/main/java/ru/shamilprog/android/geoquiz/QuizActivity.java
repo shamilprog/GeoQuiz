a325bc2a5475fe637193a86cda70cc0a09c88e28
Shamilprog <shamilprog@yandex.ru>
2020-10-13 04:04:40 +0300

    Making next button working

--- app/src/main/java/ru/shamilprog/android/geoquiz/QuizActivity.java
+++ app/src/main/java/ru/shamilprog/android/geoquiz/QuizActivity.java
@@ -31,8 +31,6 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
-        int question = mQuestionBank[mCurrentIndex].getTextResId();
-        mQuestionTextView.setText(question);
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -52,5 +50,21 @@ public class QuizActivity extends AppCompatActivity {
                                 Toast.LENGTH_SHORT).show();
             }
         });
+
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        updateQuestion();
+    }
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
     }
 }

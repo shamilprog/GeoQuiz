a71fbfeb2a821af64871b9eadf89ff89a614390b
Shamilprog <shamilprog@yandex.ru>
2020-10-19 00:44:13 +0300

    Creating a cheat button

--- app/src/main/java/ru/shamilprog/android/geoquiz/QuizActivity.java
+++ app/src/main/java/ru/shamilprog/android/geoquiz/QuizActivity.java
@@ -18,6 +18,7 @@ public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mCheatButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[] {
@@ -64,6 +65,14 @@ public class QuizActivity extends AppCompatActivity {
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
                 updateQuestion();
+            }
+        });
+
+        mCheatButton = (Button) findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
 
